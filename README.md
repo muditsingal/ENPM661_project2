c9a5264f78fb50974cc25b00906a21b63e72d362
muditsingal <muditsingal@gmail.com>
2023-03-13 17:29:35 -0400

    Final code with comments and correct equations

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ENPM661_project2
 Github repo for project 2 of ENPM661 - Planning for Autonomous Robots course
+
+### General note:
+I have used a hash map to store the node and their parameters as per the pixels in the canvas. This reduces the time required to check if an element is in open list from O(n) to O(1), which improves performance by upto 20 times. The data structure used in an 3D array where the first 2 dimensions represent the y and x coordinates of the image pixel locations, and the 3rd dimension represents the parameters of each node element, such as ctc, is_opened, is_closed and the node object itself.
+
+### Libraries:
+OpenCV, Numpy, time
+
+### Instructions to run:
+1. Run the file using console/terminal or IDE that supports user input.
+2. The code will prompt the user 4 times for 4 inputs corresponding to x and y pixel values for start and goal nodes.
+3. Enter x, y coordinates for start and goal nodes. If these are invalid, i.e. in obstacle space or out of bounds, then the code will print that the input is invalid and the program will exit.
+4. After entering valid coordinates, the code will compute the path from start node to goal node, generate 2 videos, one for path animation and another for node exploration.
+5. The code will display the final image containing the start node, goal node, and a valid path between them. Press any key on image window to exit the program.
+6. (Optional) You can disable the displaying of start and goal nodes by setting the 'visualize_start_n_goal' as False.
--- dijkstra_mudit_singal.py
+++ dijkstra_mudit_singal.py
@@ -1,41 +1,60 @@
+# Importing all the necessary libraries
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import heapq
-from queue import PriorityQueue
 import time
 
-start = time.time()
-
+# Creating 2 empty lists for storing the list of open nodes and list of nodes in path from start position to goal position
 open_list = []
 path_list = []
-# open_list = PriorityQueue()
-# closed_list = []
 
-# x = input("Please enter start x position")
-# y = input("Please enter start y position")
-# x_i = [x,y]
-# x = input("Please enter goal x position")
-# y = input("Please enter goal y position")
-# x_g = [y,x]
+# Reading the start and goal position from the user
+# x = int(input("Please enter start x position: "))
+# y = int(input("Please enter start y position: "))
+# x_i = np.array([y,x])
+# x = int(input("Please enter goal x position: "))
+# y = int(input("Please enter goal y position: "))
+# x_g = np.array([y,x])
 
+# Start recording the time to check time of execution
+start = time.time()
+
+# Test Start and goal position
+x_i = np.array([15,16])
+x_g = np.array([35, 582])
+# x_g = np.array([235, 580])
+
+# Flag to enable start and goal position visualization
 visualize_start_n_goal = True
+
+# Threshold on the number of iterations, not used currently, ignore
 n_iter = 10000
 
 # Variable that will keep count of iterations
 iters = 0 
 
+# Limits on x and y positions of the pixel nodes to prevent out-of-index access 
 x_min = 0
 x_max = 600
 
 y_min = 0
 y_max = 250
 
-video_writer_fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
-# video_writer_fourcc = cv2.VideoWriter_fourcc(*'MPEG') 
-# Creating a VideoWriter object
-video = cv2.VideoWriter('path_animation.avi', video_writer_fourcc, 30, (600, 250))
+# Checking if start node and goal node are in image space
+if x_i[1] < x_min or x_i[1] > x_max or x_i[0] < y_min or x_i[0] > y_max or x_g[1] < x_min or x_g[1] > x_max or x_g[0] < y_min or x_g[0] > y_max:
+	print("Out of bound position. Not valid!")
+	print("Exiting program")
+	exit()
 
+# Creating video codecs with XVID format writer to write 2 videos 
+video_writer_fourcc = cv2.VideoWriter_fourcc(*'XVID') 
+video_writer_fourcc2 = cv2.VideoWriter_fourcc(*'XVID') 
+# Creating a VideoWriter object
+
+# Creating 2 video writers, one for path animation and the second for storing the explored nodes
+video = cv2.VideoWriter('path_animation.avi', video_writer_fourcc, 30, (600, 250))
+explore_video = cv2.VideoWriter('explore_animation.avi', video_writer_fourcc2, 60, (600, 250))
+
+# Creating a node class to store all node parameters, including the data (node position), list of children, parent of the node, and cost to come for it
 class node:
 	def __init__(self, data):
 		self.data = data
@@ -47,12 +66,7 @@ class node:
 		child.parent = self
 		self.children.append(child)
 
-
-	def update_parent(self, parent, next_ctc):
-		self.parent = parent
-		self.ctc = parent.ctc + next_ctc
-
-
+# Function to check if move up is valid and return the next pixel location, relative cost to come from previous node and if the next move is valid 
 def move_up(node):
 	curr_px = node.data # (y,x)
 	y = curr_px[0]
@@ -66,7 +80,7 @@ def move_up(node):
 	else:
 		return [], np.inf, False
 
-
+# Function to check if move down is valid and return the next pixel location, relative cost to come from previous node and if the next move is valid
 def move_down(node):
 	curr_px = node.data # (x,y)
 	y = curr_px[0]
@@ -79,7 +93,7 @@ def move_down(node):
 	else:
 		return [], np.inf, False
 
-
+# Function to check if move left is valid and return the next pixel location, relative cost to come from previous node and if the next move is valid
 def move_left(node):
 	curr_px = node.data # (x,y)
 	y = curr_px[0]
@@ -92,7 +106,7 @@ def move_left(node):
 	else:
 		return [], np.inf, False
 
-
+# Function to check if move right is valid and return the next pixel location, relative cost to come from previous node and if the next move is valid
 def move_right(node):
 	curr_px = node.data # (x,y)
 	y = curr_px[0]
@@ -105,12 +119,12 @@ def move_right(node):
 	else:
 		return [], np.inf, False
 
-
+# Function to check if move up-left is valid and return the next pixel location, relative cost to come from previous node and if the next move is valid
 def move_up_left(node):
 	curr_px = node.data # (x,y)
 	y = curr_px[0]
 	x = curr_px[1]
-	if y > y_max and x < x_min:
+	if y > y_max-2 or x < x_min+1:
 		return [], np.inf, False
 
 	if img[y+1, x-1, 1] == 255:
@@ -118,11 +132,12 @@ def move_up_left(node):
 	else:
 		return [], np.inf, False
 
+# Function to check if move up-right is valid and return the next pixel location, relative cost to come from previous node and if the next move is valid
 def move_up_right(node):
 	curr_px = node.data # (x,y)
 	y = curr_px[0]
 	x = curr_px[1]
-	if y > y_max and x > x_max:
+	if y > y_max-2 or x > x_max-2:
 		return [], np.inf, False
 
 	if img[y+1, x+1, 1] == 255:
@@ -130,12 +145,12 @@ def move_up_right(node):
 	else:
 		return [], np.inf, False
 
-
+# Function to check if move down-left is valid and return the next pixel location, relative cost to come from previous node and if the next move is valid
 def move_down_left(node):
 	curr_px = node.data # (x,y)
 	y = curr_px[0]
 	x = curr_px[1]
-	if y < y_min or x < x_min:
+	if y < y_min+1 or x < x_min+1:
 		return [], np.inf, False
 
 	if img[y-1, x-1, 1] == 255:
@@ -143,12 +158,12 @@ def move_down_left(node):
 	else:
 		return [], np.inf, False
 
-
+# Function to check if move down-right is valid and return the next pixel location, relative cost to come from previous node and if the next move is valid
 def move_down_right(node):
 	curr_px = node.data # (x,y)
 	y = curr_px[0]
 	x = curr_px[1]
-	if y < y_min and x > x_max:
+	if y < y_min+1 or x > x_max-2:
 		return [], np.inf, False
 
 	if img[y-1, x+1, 1] == 255:
@@ -156,137 +171,164 @@ def move_down_right(node):
 	else:
 		return [], np.inf, False
 
-x_i = np.array([15,16])
-x_g = np.array([35, 582])
-# x_g = np.array([235, 580])
+# Creating a list of all the move functions for easy iteration and reducing code length
 moves_list = [move_up, move_down, move_left, move_right, move_up_left, move_up_right, move_down_left, move_down_right]
+
+# Creating an array of nodes with 4 parameters: The node at that location, cost-to-come to that pixel from start node, flag that indicates if the node is in closed list, flag that indicates if the node is in the open list
 node_arr = np.empty(shape=[250, 600, 4], dtype=object)
 node_arr[:, :, 0] = None		# Node 
 node_arr[:, :, 1] = np.inf		# Cost to come of current pixel
 node_arr[:, :, 2] = False		# In closed list
 node_arr[:, :, 3] = False		# In open list
 
+# Creating the start and goal nodes. Initializing the start node's cost-to-come as 0
 start_node = node(x_i)
 start_node.ctc = 0
 goal_node = node(x_g)
 
-# Initializing the start node cost to some as 0
+# Setting start node's cost-to-come in node array as 0 and also, the node at the starting position as the start node
 node_arr[x_i[0], x_i[1], 1] = 0
 node_arr[x_i[0], x_i[1], 0] = start_node
 
+# Initializing the blank canvas using numpy ones function
 img = np.ones([250, 600, 3])*255
 
+# Creating a bloat grid that will be used to detect pixels that are in obstacle space, and color the pixels blue that represent the bloated region
 bloat_grid = np.ones([11,11,3])*255
 bloat_grid[:,:,1:3] = 0
 
-# Drawing the rectangular obstacles
-img[0:100, 100:150, :] = 0
-img[-100:-1, 100:150, :] = 0
+# Drawing the obstacles in image space as black pixels, first line is for rectangles, 2nd one is for triangle, 3rd is for hexagon
+x_px = 0
+y_px = 0
+while x_px < x_max:
+	y_px = 0
+	while y_px < y_max:
+		if ((y_px>=0 and y_px<=100 and x_px>=100 and x_px<=150) or (y_px >= 150 and y_px <= 250 and x_px>=100 and x_px<=150) or 
+		   (y_px >= 2*x_px - 895 and y_px <= -2*x_px + 1145 and x_px >= 460) or
+		   ((x_px >= 235) and (x_px <= 365) and ((0.6538*x_px + y_px) >= 246.1538) and ((-0.6538*x_px + y_px) >= -146.1538) and ((-0.6538*x_px + y_px) <= 3.8461) and ((0.6538*x_px + y_px) <= 396.1538) ) ):
+			img[y_px, x_px] = np.array([0,0,0], dtype=np.uint8)
 
-# Polygon corner points coordinates
-hex_pts = np.array([[300, 200], [300+65, 125+32],
-                	[300+65, 125-32], [300, 50],
-                	[300-65, 125-32], [300-65, 125+32]], np.int32)
- 
- 
-tri_pts = np.array([[460, 225], [510, 125], [460, 25]])
+		y_px += 1
 
-color = (0, 0, 0)
-# img = cv2.polylines(img, np.array([hex_pts]), True, color, 5)
-img = cv2.fillPoly(img, np.array([hex_pts]), color)
-img = cv2.fillPoly(img, np.array([tri_pts]), color)
+	x_px += 1
 
+# Running the bloat grid throughout the canvas and taking bitwise and to get the bloated space
 for i in range(5,img.shape[0]-5):
 	for j in range(5,img.shape[1]-5):
 		if np.sum(img[i,j]) == 0:
-			# print(i,j)
-			# print(img[i-5:i+5, j-5:j+5].shape)
-			# print(bloat_grid.shape)
 			img[i-5:i+6, j-5:j+6] = cv2.bitwise_and(img[i-5:i+6, j-5:j+6, :], bloat_grid)
 
+# Adding the 5px bloated regions at the edges of canvas that can't be taken care of from the obstacle space logic
 img[:, 0:6] = cv2.bitwise_and(img[:, 0:6], np.array([255, 0, 0]) )
 img[:, -5:] = cv2.bitwise_and(img[:, -5:], np.array([255, 0, 0]) )
 img[0:6, :] = cv2.bitwise_and(img[0:6, :], np.array([255, 0, 0]) )
 img[-5:, :] = cv2.bitwise_and(img[-5:, :], np.array([255, 0, 0]) )
+img = img.astype(np.uint8)
 
+# Checking if start node and goal node are in valid locations
+if img[x_i[0], x_i[1], 1] == 0:
+	print("Start position not valid!")
+	print("Exiting program")
+	exit()
 
+if img[x_g[0], x_g[1], 1] == 0:
+	print("Goal position not valid!")
+	print("Exiting program")
+	exit()
+
+# If visualization of start and goal nodes is turned on, visualize the start and goal nodes using circles in image canvas
 if visualize_start_n_goal:
-	start_color = [0, 0, 255]
-	goal_color = [0, 255, 0]
+	start_color = [0, 0, 255] 	# Start node as red color
+	goal_color = [0, 255, 0] 	# Goal node as Green color
 	cv2.circle(img, (x_i[1], x_i[0]) , color=start_color, radius=3, thickness=-1)
 	cv2.circle(img, (x_g[1], x_g[0]) , color=goal_color, radius=3, thickness=-1)
 	img = cv2.flip(img, flipCode=0)
-	print(img.shape)
 
+# Starting the dijkstra algorithm by assiging the start node as current node and appending the start node in open list
 curr_node = start_node
-# heapq.heappush(open_list, [0, start_node.data])
-# open_list.put([0, start_node])
 open_list.append([0, start_node])
 
+# Taking copy of image for writing to the exploration video submission
+og_img = img.copy()
+explore_video.write(og_img)
+
+# Main loop of the dijkstra's algorithm that continues until the current node is pointing to the goal node and until the open list is not empty
 while np.count_nonzero(curr_node.data - goal_node.data) != 0 and len(open_list) != 0 :
-	# [curr_ctc, curr_node_data] = heapq.heappop(open_list)
+	# Read the node with least cost-to-come in open list 
 	[curr_ctc, curr_node] = open_list.pop(0)
-	# heapq.heappush(closed_list, [curr_ctc, curr_node_data])
+	# Marking the node as is_in_opened_list in the node array
 	node_arr[curr_node.data[0], curr_node.data[1], 2] = True
 
 	iters += 1
 
-
+	# Check new node for every possible move
 	for move in moves_list:
-		# [x_new, y_new], incr_ctc, success = move(curr)
 		new_coords, incr_ctc, success = move(curr_node)
-		# cv2.circle(img, (new_coords[1]+3, new_coords[0]+3) , color=start_color, radius=1, thickness=-1)
 
+		# If the move is possible do the following, else check other moves
 		if success:
+			# Read the node parameters at the location generated from the above move
 			new_node_params = node_arr[new_coords[0], new_coords[1]]
+
+			# Do the following if the node is not in closed list
 			if new_node_params[2] == False:
+				# Do the following if the node is not in open list or the cost-to-come of current node is infinity
 				if new_node_params[3] == False or new_node_params[1] == np.inf:
+					# Modifying the image that is used to record the explored nodes and writing the modified frames to the explored-nodes video
+					og_img[y_max - new_coords[0], new_coords[1]] = np.array([255, 255, 0])
+					explore_video.write(og_img)
+
+					# Create a new node object at the coordinates returned by the move function and assign it the correct cost-to-come
 					temp_node = node(new_coords)
 					temp_node.ctc = curr_node.ctc + incr_ctc
 					node_arr[new_coords[0], new_coords[1], 1] = curr_node.ctc + incr_ctc
 
+					# Appending the new node as child of current node and also assigning the parent of new node as the current node
 					curr_node.append_child(temp_node)
 					node_arr[new_coords[0], new_coords[1], 0] = temp_node
 
 					# Adding the new node in open list and marking it as open in the array of nodes
-					# open_list.put([1, temp_node])
 					open_list.append([curr_node.ctc + incr_ctc, temp_node])
-					open_list.sort(key=lambda x:x[0])
-					# heapq.heappush(open_list, [curr_node.ctc + incr_ctc, new_coords])
+
+					# Sorting the open-list with respect to cost-to-come
+					open_list.sort(key=lambda x:x[0]) 
 					node_arr[new_coords[0], new_coords[1], 3] = True
 
-
+				# If the old cost-to-come is greater than cost-to-come of parent node + the next cost-to-come, update the next node's parent and cost-to-come
 				elif node_arr[new_coords[0], new_coords[1], 1] > curr_node.ctc + incr_ctc:
+					# Updating the parent of next-node
 					node_arr[new_coords[0], new_coords[1], 0].parent = curr_node
+
+					# Updating the cost-to-come of next-node
 					node_arr[new_coords[0], new_coords[1], 0].ctc = curr_node.ctc + incr_ctc
 					node_arr[new_coords[0], new_coords[1], 1] = curr_node.ctc + incr_ctc
 
-
-
-print(curr_node.data)
+# Running back-tracking from the end-node to the start node
 back_parent = curr_node
 while back_parent != None:
 	path_list.append(back_parent.data)
 	back_parent = back_parent.parent
 
+# Reverse the path list to get a sorted list from start-node to end-node
 path_list = path_list[::-1]	
+img = img.astype(np.uint8)
 
+# Draw the path as black pixels on the image canvas
 for i in range(len(path_list)):
-	# print(path_list[i, 0])
 	img[y_max - path_list[i][0], path_list[i][1]] = np.array([0, 0, 0])
 	video.write(img)
-	# video.write(cv2.resize(img, dsize=(600, 250)))
-
 
 video.release()
+explore_video.release()
 
-print(iters)
+# Print the number of iterations taken
+print("Number of iterations taken: ", iters)
 
+# Finding and printing the total time taken by the code to generate the output and the related files
 end = time.time()
-
 print("Total time taken: ", end - start)
 
-
+# Display the final image with path
 cv2.imshow('img', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
